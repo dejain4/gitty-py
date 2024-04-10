2a6efc396ff14fb02481aac7cf12a0ef489ebb5d
Deepika Jain <dejain4@gmail.com>
2024-04-10 14:59:26 +0100

    Add publish

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="gitty_py",
+    version="0.1",
+    description="A sample Python package",
+    author="Deepika",
+    author_email="dejain4@gmail.com",
+    packages=["gitty_py"],
+    install_requires=["flask"],
+)
